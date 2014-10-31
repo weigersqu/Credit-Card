0facda1146e1dc9b50e1d681aa4106152c0146e9
Q <Q@192.168.0.12>
2014-10-31 06:58:55 -0600

    Last Commit

--- Credit Card/src/CreditCard.java
+++ Credit Card/src/CreditCard.java
@@ -6,32 +6,98 @@ import java.util.Scanner;
 
 public class CreditCard
 	
-	{
-	static long [] myArray = new long [16];
+{
+
+	public static long userCreditCardNumber;
 	public static void main(String[] args)
 		{
-		Scanner userInput = new Scanner( System.in);
+		askUser();
+		validation();
 		
-		long cardNum;
-		System.out.println("Please enter your credit card number");
-		cardNum = userInput.nextLong();
-		
-		for(int i = 0; i < 16; i++)
-			{
-		long strippedDigit = cardNum % 10;
-		myArray [i] = strippedDigit;
-		cardNum = cardNum / 10;
-			}
-		
-				
-				
-		
-			
-
 		}
 
-	}
+		public static long askUser()
+			{
+			
+			System.out.println("Please input a 16 digit credit card number");
+			Scanner userInput = new Scanner (System.in);
+			userCreditCardNumber = userInput.nextLong();
+	
+			return userCreditCardNumber;
+			
+			
+			}
 
+		public static void validation()
+			{
+			
+			long[] creditCardArray = new long [16];
+
+			for (int i =15; i > -1; i--)
+				{
+				creditCardArray[i] = userCreditCardNumber %10;
+				userCreditCardNumber = userCreditCardNumber/10;
+				}
+			
+			System.out.println();
+		
+			
+			long newDoubleNumber =0;
+			
+			for (int i=0; i< 16; i=i+2)
+				{
+				newDoubleNumber = creditCardArray[i];
+				long newNumber = newDoubleNumber *2;
+
+
+				creditCardArray[i] = newNumber;
+				}
+			
+			System.out.println();
+			
+			
+			for (int i = 0; i < 16; i=i+2)
+				{
+				if ( creditCardArray[i] >= 10)
+					{
+					long sum = 0;
+					long number = creditCardArray[i];
+					
+				for (int k = 0; k <2; k++)
+					{
+					long digit = number %10;
+					number = number /10;
+					sum = sum + digit;
+					}
+				creditCardArray[i] = sum;
+				}
+			
+				}
+			
+			System.out.println();
+			
+			
+			long numberDigit = 0;
+			long totalSum = 0;
+			
+			for (int i = 0; i <16; i++)
+				{
+				totalSum = totalSum + creditCardArray[i];
+				}
+			if (totalSum %10 ==0)
+			{
+			System.out.println("This credit card is valid ");
+			}
+			else
+				{
+				System.out.print("This credit card is invalid");
+				}
+			
+			
+			
+			
+			}
+	}
 
 // for loop 1-16
 //    {
